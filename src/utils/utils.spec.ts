dc71fa63a9995c3d980922d08b075b72550b06ce
fournier-alexis <75725046+fournier-alexis@users.noreply.github.com>
2021-06-25 13:52:58 +0200

    Add files via upload

--- src/utils/utils.spec.ts
+++ src/utils/utils.spec.ts
@@ -4,5 +4,6 @@ describe("Verify utils functions", () => {
     it("should return the right result", () => {
         expect(add(1, 2)).toBe(3);
         expect(add(0, 0)).toBe(0);
+        expect(add(0,1)).toBe(0);
     })
 })
